6be8f796175b7d7d464de3021c7ad993738a288e
FeGroders <fernandogroder@gmail.com>
2021-08-20 17:37:35 -0300

    init

---
+++ main.js
@@ -0,0 +1,29 @@
+//send notification to user
+let enableNotification = false;
+
+function onClickBtn(){
+    if (!enableNotification) {
+        enableNotification = true;
+        sendNotification('Hora da água','Bora beber uma águinha meu patrão');
+    } else {
+        enableNotification = false;
+    }
+}
+
+function sendNotification(title, body, url) {
+    if (Notification.permission !== "granted"){
+        Notification.requestPermission();
+    } else {
+        enableNotification = true;
+        var notification = new Notification(title, {
+            icon: "./assets/agua-mineral.png",
+            body: body,
+        });
+
+        notification.onclick = function () {
+            window.open(url);
+        };
+
+        setTimeout(notification.close.bind(notification), 5000);
+    }
+}
